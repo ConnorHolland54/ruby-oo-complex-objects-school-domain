556a85712093e459379085dc019aaca9a7db0ebc
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-30 17:10:03 -0700

    commit tree d6f39f7b370568bce14b28df06570769309195e2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @school = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = [name]
+        else
+            @roster[grade] << name
+        end    
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+       
+        @roster.each do |key, value|
+            sorted_value = value.sort
+            @roster[key] = sorted_value
+        end
+
+        return @roster
+
+
+
+
+    end
+
+
+end
